394b899a08d46c1505675140084f88fc67d591a3
ariwaranosai <nkssai@outlook.com>
2016-03-23 19:13:17 +0800

    add unit test

--- src/test/scala/nozomi/nzmlib/crowdsourcing/VotingTest.scala
+++ src/test/scala/nozomi/nzmlib/crowdsourcing/VotingTest.scala
@@ -8,6 +8,25 @@ import org.scalatest.FlatSpec
   */
 
 class VotingTest extends FlatSpec {
+    "Voting" must "get correct solution" in {
+        val data = List[LabeledData](
+            LabeledData(0, 1, 1),
+            LabeledData(0, 2, 0),
+            LabeledData(0, 0, 0),
+            LabeledData(1, 1, 1),
+            LabeledData(1, 0, 1),
+            LabeledData(2, 2, 0)
+        )
 
-    // TODO
+        val crowd = new Voting().setDefaultLabel(-1).run(data)
+
+        assert(crowd.getSolutionById(0) == 0)
+        assert(crowd.getSolutionById(1) == 1)
+        assert(crowd.getSolutionById(2) == 0)
+
+        assert(crowd.getReliabilityById(0) == 1)
+        assert(crowd.getReliabilityById(1) == 0.5)
+        assert(crowd.getReliabilityById(2) == 1)
+    }
+
 }
