73577e54c268c962830886d76601edb0daaec569
ariwaranosai <nkssai@outlook.com>
2016-03-28 19:37:01 +0800

    add GLAD

---
+++ src/main/scala/nozomi/nzmlib/crowdsourcing/GLAD.scala
@@ -0,0 +1,18 @@
+package nozomi.nzmlib.crowdsourcing
+
+import nozomi.util.NZMLogging
+
+/**
+  * Created by ariwaranosai on 16/3/28.
+  *
+  */
+
+class GLAD extends GeneralizedCSAlgorithm[OrdinaryCSModel]
+    with NZMLogging {
+
+    var epsilon: Double = 0.5
+
+    override var optimizer: Seq[LabeledData] => (Seq[Double], Seq[Double]) = _
+
+    override protected def createModel(solution: Seq[Double], workers: Seq[Double]): OrdinaryCSModel = ???
+}
