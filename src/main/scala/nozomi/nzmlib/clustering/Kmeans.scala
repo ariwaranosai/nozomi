2ddba0c7f31afba0089a10b672ce24a388551138
ariwaranosai <nkssai@outlook.com>
2016-08-04 17:16:02 +0800

    kmeans|| with random init

--- src/main/scala/nozomi/nzmlib/clustering/Kmeans.scala
+++ src/main/scala/nozomi/nzmlib/clustering/Kmeans.scala
@@ -3,6 +3,7 @@ package nozomi.nzmlib.clustering
 import breeze.linalg
 import breeze.linalg.DenseVector
 import nozomi.nzmlib.mlutil.DataSet._
+import nozomi.util.NZMLogging
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
@@ -19,22 +20,39 @@ class Kmeans(
               private var epsilon: Double,
               private var initializationStep: Int,
               private var seed: Long
-            ) {
+            ) extends NZMLogging {
 
 
+  private def initRandom(data: Seq[VectorWithNorm]): Array[Array[VectorWithNorm]] = {
+    val sample = data.simple(runs * k, Random.nextLong())
+
+    Array.tabulate(runs)(r => sample.slice(r * k, (r + 1) * k).map {
+      v => VectorWithNorm(DenseVector(v.vector.data), v.norm)
+    }.toArray)
+  }
+
+  /**
+    * 采用 kmeans||的初始化方法
+    * 论文 http://theory.stanford.edu/~sergei/papers/vldb12-kmpar.pdf
+    * 具体解释可以看 http://stats.stackexchange.com/questions/135656/k-means-a-k-a-scalable-k-means
+    *
+    * @param data data to kmeans
+    * @return centers vector of runs times
+    */
   private def initKmeansll(data: Seq[VectorWithNorm]): Array[Array[VectorWithNorm]] = {
+    logger.info(" initing ")
+
     // 预先构造好运行次数的中心点,以及对应的costs,每次计算同时进行
     val centers = Array.tabulate(runs)(r => ArrayBuffer.empty[VectorWithNorm])
     var costs = data.map(_ => Array.fill(runs)(Double.PositiveInfinity))
-
 
     val seed = util.Random
     seed.setSeed(this.seed)
 
     val sample = data.simple(runs, seed.nextLong)
     require(sample.size >= runs, s"Required $runs samples but got ${sample.size}")
+    // 为每一次run,取样第一个点
     val newCenters = Array.tabulate(runs)(r => ArrayBuffer(sample(r)))
-
 
     def mergeNewCenters(): Unit = {
       var r = 0
@@ -45,19 +63,21 @@ class Kmeans(
       }
     }
 
-    //
+    // 这个地方可以指定,也可以通过计算那个\psi来得到
     var step = 0
     while (step < this.initializationStep) {
       val preCosts = costs
 
-      //
-      costs = data.zip(preCosts).map { case (point, cost) =>
+      // 对于每一个数据点,分别计算它在每个run中靠集合C,最近的点的距离。但是由于
+      // 只有一个元素,所以就算一个就行了
+      costs = data.zip(preCosts).par.map { case (point, cost) =>
         Array.tabulate(runs) { r =>
           math.min(Kmeans.pointCost(newCenters(r), point), cost(r))
         }
-      }
+      }.to
 
 
+      // 计算那个psi的sum,用作概率的归一化项
       val sumCosts = costs.par.aggregate(new Array[Double](runs))(
         seqop = (s, v) => {
           var r = 0
@@ -77,7 +97,8 @@ class Kmeans(
         }
       )
 
-      val chosen = data.zip(costs).zipWithIndex.map {
+      // 随机选择 令l=2*k
+      val chosen = data.zip(costs).par.zipWithIndex.map {
         case ((p, c), index) =>
           val rand = new Random()
           rand.setSeed(seed.nextInt ^ (step << 16) ^ index)
@@ -87,11 +108,14 @@ class Kmeans(
 
           if (rs.nonEmpty) Some((p, rs)) else None
       }
+
+      // 把newCenters里面的加到centers里面去
       mergeNewCenters()
 
-      // todo Some is not correct
+      // 把新选出来的newCenters加到centers里面去
       chosen.foreach { case Some((p, rs)) =>
-          rs.foreach(newCenters(_) += p)
+        rs.foreach(newCenters(_) += p)
+      case None =>
       }
 
       step += 1
@@ -100,8 +124,8 @@ class Kmeans(
 
     mergeNewCenters()
 
-    // we may get more than k candidate centers for each run
-    val weightMap = data.par.flatMap { p =>
+    // 这样每一次run都有很多的
+    val weightMap = data.flatMap { p =>
       Iterator.tabulate(runs) {
         r => ((r, Kmeans.findClosest(centers(r), p)._1), 1.0)
       }
@@ -109,18 +133,26 @@ class Kmeans(
       case (k, ys) => (k, ys.map(_._2).sum)
     }
 
-    val findalCenters = (0 until runs).par.map {
+    logger.info(" in kmeans++ ")
+
+    val finalCenters = (0 until runs).map {
       r =>
         val myCenters = centers(r).toArray
         val myWeights = myCenters.indices.map(i => weightMap.getOrElse((r, i), 0.0)).toArray
         Kmeans.KmeansPlusPlus(r, myCenters, myWeights, k, 30)
     }
 
-
-    centers.map(x => x.toArray)
-
-
+    finalCenters.toArray
   }
+
+  private var initialModel: Option[KmeansModel] = None
+
+  def setInitialModel(model: KmeansModel): this.type = {
+    require(model.k == k, " mismatched cluster count")
+    initialModel = Some(model)
+    this
+  }
+
 
 }
 
@@ -131,14 +163,15 @@ class Kmeans(
   * @param norm   vector's norm
   */
 private[clustering]
-class VectorWithNorm(val vector: DenseVector[Double], val norm: Double) extends Serializable {
+case class VectorWithNorm(vector: DenseVector[Double], norm: Double) extends Serializable {
   def this(vector: DenseVector[Double]) = this(vector, linalg.norm(vector))
-
   def this(array: Array[Double]) = this(new DenseVector[Double](array))
+
+  override def toString: String = vector.toString()
 }
 
 
-object Kmeans {
+object Kmeans extends NZMLogging {
   private[nzmlib] def pointCost(
                                  centers: TraversableOnce[VectorWithNorm],
                                  point: VectorWithNorm
@@ -172,12 +205,102 @@ object Kmeans {
     (bestIndex, bestDistance)
   }
 
-  // todo Kmeans++
+  private[clustering]
   def KmeansPlusPlus(
-                    seed: Long,
-                    points: Array[VectorWithNorm],
-                    weights: Array[Double],
-                    k: Int,
-                    maxIterations: Int
-                    ): Array[VectorWithNorm] = ???
+                      seed: Long,
+                      points: Array[VectorWithNorm],
+                      weights: Array[Double],
+                      k: Int,
+                      maxIterations: Int
+                    ): Array[VectorWithNorm] = {
+
+
+    val rand = new Random(seed)
+    val dim: Int = points(0).vector.length
+
+    val centers = new Array[VectorWithNorm](k)
+
+    centers(0) = pickWeighted(rand, points, weights)
+    // 预先计算每一个点到第一个点的距离
+    val costArray = points.map(x => x.vector.dot(centers(0).vector))
+
+    for (i <- 1 until k) {
+      // 取随机数
+      val sum = costArray.zip(weights).map(p => p._1 * p._2).sum
+      val r = rand.nextDouble() * sum
+      var cumulativeScore = 0.0
+      var j = 0
+      while (j < points.length && cumulativeScore < r) {
+        cumulativeScore += weights(j) * costArray(j)
+        j += 1
+      }
+
+      if (j == 0) {
+        logger.warn("kmeans++ initialization ran out of disinct points for centers." +
+          s" Using deplicate point for k = $i.")
+        centers(i) = points(0)
+      } else {
+        centers(i) = points(j - 1)
+      }
+
+      for (p <- points.indices) {
+        costArray(p) = math.min(points(p).vector.dot(centers(i).vector), costArray(p))
+      }
+    }
+
+    // Lloyd's algorithm kmeans
+    val oldClosest = Array.fill(points.length)(-1)
+    var iteration = 0
+    var moved = true
+    while (moved && iteration < maxIterations) {
+      moved = false
+      val counts = Array.fill(k)(0.0)
+      val sums = Array.fill(k)(DenseVector.zeros[Double](dim))
+      var i = 0
+      while (i < points.length) {
+        val p = points(i)
+        val index = Kmeans.findClosest(centers, p)._1
+        sums(index) += weights(i) * p.vector
+        counts(index) += weights(i)
+        if (index != oldClosest(i)) {
+          moved = true
+          oldClosest(i) = index
+        }
+
+        i += 1
+      }
+
+      // 更新中心点
+      var j = 0
+      while (j < k) {
+        if (counts(j) == 0.0) {
+          centers(j) = points(rand.nextInt(points.length))
+        } else {
+          sums(j) *= 1.0 / counts(j)
+        }
+        j += 1
+      }
+
+      iteration += 1
+    }
+
+    if (iteration == maxIterations) {
+      logger.info(s" kmeans++ reached the max number of iterations: $maxIterations.")
+    } else {
+      logger.info(s" kmeans++ converged in $iteration iterations.")
+    }
+
+    centers
+  }
+
+  private def pickWeighted[T](rand: Random, data: Array[T], weights: Array[Double]): T = {
+    val r = rand.nextDouble() * weights.sum
+    var i = 0
+    var curWeight = 0.0
+    while (i < data.length && curWeight < r) {
+      curWeight += weights(i)
+      i += 1
+    }
+    data(i - 1)
+  }
 }
---
+++ src/test/scala/nozomi/nzmlib/clustering/KmeansTest.scala
@@ -0,0 +1,16 @@
+package nozomi.nzmlib.clustering
+
+import breeze.linalg
+import breeze.linalg.DenseVector
+import org.scalatest.FlatSpec
+
+/**
+  * Created by ariwaranosai on 16/8/4.
+  *
+  */
+
+class KmeansTest extends FlatSpec {
+  "kmeans||" must "can run without warning" in {
+  }
+
+}
